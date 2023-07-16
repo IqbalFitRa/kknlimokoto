93a97187ddde4430668674f8b2f4e69e4254ab23
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2023-07-16 09:30:20 +0700

    ualng

---
+++ user.php
@@ -0,0 +1,24 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="kkn-limokoto";
+
+$koneksi=mysqli_connect($host,$user,$pass,$db);
+if(!$koneksi){
+    die("tidak terhubung");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Surat Rekomendasi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
